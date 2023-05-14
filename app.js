8fd45bfa452c7d0c32f2d1df941b1209177856b9
Viktor Zbodulja <viktorzbodulja@gmail.com>
2023-05-14 22:17:28 +0200

    responsivity added

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ function animateSlides() {
     slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
     //create Scene
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
@@ -155,6 +154,13 @@ barba.init({
           { x: "100%", stagger: 0.25, onComplete: done }
         );
         tl.fromTo(data.next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav_header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2inOut" },
+          "-=1.5"
+        );
       },
     },
   ],
